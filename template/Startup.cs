cbae7988353ec07cfc93371b5781bf45736eccb2
ManPavel <coolerqman@gmail.com>
2015-05-02 11:52:53 +0300

    Made a template of the home page.

---
+++ template/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(template.Startup))]
+namespace template
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
